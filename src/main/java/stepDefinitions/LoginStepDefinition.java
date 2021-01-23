e2f2a866f1e48c0bf4b40e9a85c2671488fed347
muhammadfarhansoomro <61628268+muhammadfarhansoomro@users.noreply.github.com>
2021-01-23 09:48:02 +0100

    Add files via upload

---
+++ src/main/java/stepDefinitions/LoginStepDefinition.java
@@ -0,0 +1,96 @@
+package stepDefinitions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.testng.asserts.SoftAssert;
+
+public class LoginStepDefinition{
+
+	 WebDriver driver;
+	SoftAssert softAssert;
+
+	
+	 @Given("^user is already on Login Page$")
+	 public void user_already_on_login_page(){
+	 System.setProperty("webdriver.chrome.driver","C:\\Users\\farhan.soomro\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	 driver = new ChromeDriver();
+		 driver.get("https://ui.cogmento.com/");
+	 //driver.get("https://www.freecrm.com/index.html");
+		 softAssert= new SoftAssert();
+	 }
+	
+	
+	 @When("^title of login page is Free CRM$")
+	 public void title_of_login_page_is_free_CRM(){
+	 String title = driver.getTitle();
+	 System.out.println(title);
+		 softAssert.assertEquals("Free CRM in the cloud software boosts sales", title);
+	 //Assert.assertEquals("Free CRM in the cloud software boosts sales", title);
+	 }
+	
+	 //Reg Exp:
+	 //1. \"([^\"]*)\"
+	 //2. \"(.*)\"
+	
+	 @Then("^user enters \"(.*)\" and \"(.*)\"$")
+	 public void user_enters_username_and_password(String username, String password){
+	 	//driver.findElement(By.xpath("/html/body/div[2]/header/div/nav/div[2]/div/a")).click();
+	 	driver.findElement(By.name("email")).sendKeys(username);
+	 	driver.findElement(By.name("password")).sendKeys(password);
+	 }
+	
+	 @Then("^user clicks on login button$")
+	 public void user_clicks_on_login_button() {
+	 WebElement loginBtn =
+	 driver.findElement(By.xpath("//input[@type='submit']"));
+	 JavascriptExecutor js = (JavascriptExecutor)driver;
+	 js.executeScript("arguments[0].click();", loginBtn);
+	 }
+	
+	
+	 @Then("^user is on home page$")
+	 public void user_is_on_hopme_page(){
+	 String title = driver.getTitle();
+	 System.out.println("Home Page title ::"+ title);
+		 SoftAssert softAssert = new SoftAssert();
+		 softAssert.assertEquals("CRMPRO", title);
+		 //Assert.assertEquals("CRMPRO", title);
+	 }
+	 
+	 @Then("^user moves to new contact page$")
+	 public void user_moves_to_new_contact_page() {
+		driver.switchTo().frame("mainpanel");
+		Actions action = new Actions(driver);
+		action.moveToElement(driver.findElement(By.xpath("//a[contains(text(),'Contacts')]"))).build().perform();
+		driver.findElement(By.xpath("//a[contains(text(),'New Contact')]")).click();
+		
+		}
+	 
+	 
+	 @Then("^user enters contact details \"(.*)\" and \"(.*)\" and \"(.*)\"$")
+	 public void user_enters_contacts_details(String firstname, String lastname, String position){
+		 driver.findElement(By.id("first_name")).sendKeys(firstname);
+		 driver.findElement(By.id("surname")).sendKeys(lastname);
+		 driver.findElement(By.id("company_position")).sendKeys(position);
+		 driver.findElement(By.xpath("//*[@id=\"ui\"]/div/div/form/div/div[3]")).click();
+	 }
+	 
+
+	 @Then("^Close the browser$")
+	 public void close_the_browser(){
+		 driver.quit();
+	 }
+	
+	
+	
+
+}
---
+++ target/cucumber-reports/cucumber-pretty/report.js
@@ -0,0 +1,165 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("login.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Free CRM Login Feature",
+  "description": "",
+  "id": "free-crm-login-feature",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "line": 3,
+  "name": "Free CRM Login Test Scenario",
+  "description": "",
+  "id": "free-crm-login-feature;free-crm-login-test-scenario",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 5,
+  "name": "user is already on Login Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "title of login page is Free CRM",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "user enters \"\u003cusername\u003e\" and \"\u003cpassword\u003e\"",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 8,
+  "name": "user clicks on login button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 9,
+  "name": "user is on home page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 10,
+  "name": "Close the browser",
+  "keyword": "Then "
+});
+formatter.examples({
+  "line": 13,
+  "name": "",
+  "description": "",
+  "id": "free-crm-login-feature;free-crm-login-test-scenario;",
+  "rows": [
+    {
+      "cells": [
+        "username",
+        "password"
+      ],
+      "line": 14,
+      "id": "free-crm-login-feature;free-crm-login-test-scenario;;1"
+    },
+    {
+      "cells": [
+        "naveenk",
+        "test@123"
+      ],
+      "line": 15,
+      "id": "free-crm-login-feature;free-crm-login-test-scenario;;2"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.scenario({
+  "line": 15,
+  "name": "Free CRM Login Test Scenario",
+  "description": "",
+  "id": "free-crm-login-feature;free-crm-login-test-scenario;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 5,
+  "name": "user is already on Login Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "title of login page is Free CRM",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "user enters \"naveenk\" and \"test@123\"",
+  "matchedColumns": [
+    0,
+    1
+  ],
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 8,
+  "name": "user clicks on login button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 9,
+  "name": "user is on home page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 10,
+  "name": "Close the browser",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_already_on_login_page()"
+});
+formatter.result({
+  "duration": 24983111600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.title_of_login_page_is_free_CRM()"
+});
+formatter.result({
+  "duration": 7077300,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "naveenk",
+      "offset": 13
+    },
+    {
+      "val": "test@123",
+      "offset": 27
+    }
+  ],
+  "location": "LoginStepDefinition.user_enters_username_and_password(String,String)"
+});
+formatter.result({
+  "duration": 162793000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_clicks_on_login_button()"
+});
+formatter.result({
+  "duration": 68863800,
+  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"xpath\",\"selector\":\"//input[@type\u003d\u0027submit\u0027]\"}\n  (Session info: chrome\u003d87.0.4280.141)\nFor documentation on this error, please visit: http://seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.11.0\u0027, revision: \u0027e59cfb3\u0027, time: \u00272018-03-11T20:26:55.152Z\u0027\nSystem info: host: \u0027DESKTOP-4HLSF8M\u0027, ip: \u002710.242.2.48\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_271\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 87.0.4280.141, chrome: {chromedriverVersion: 87.0.4280.88 (89e2380a3e36c..., userDataDir: C:\\Users\\FARHAN~1.SOO\\AppDa...}, goog:chromeOptions: {debuggerAddress: localhost:52129}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:virtualAuthenticators: true}\nSession ID: cf44f3d23f0d6c7c456e02aac688c45d\n*** Element info: {Using\u003dxpath, value\u003d//input[@type\u003d\u0027submit\u0027]}\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:545)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:319)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByXPath(RemoteWebDriver.java:421)\r\n\tat org.openqa.selenium.By$ByXPath.findElement(By.java:361)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:311)\r\n\tat stepDefinitions.LoginStepDefinition.user_clicks_on_login_button(LoginStepDefinition.java:54)\r\n\tat ✽.Then user clicks on login button(login.feature:8)\r\n",
+  "status": "failed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_is_on_hopme_page()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "LoginStepDefinition.close_the_browser()"
+});
+formatter.result({
+  "status": "skipped"
+});
+});
